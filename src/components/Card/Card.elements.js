64beaac033b0c4e5a19705633f083ba2a6b9af5a
Matheus <kroskamatheus@gmail.com>
2022-03-28 00:00:04 -0300

    Layout Cadastro Nutricionista + Paciente, Raddix Setup

--- src/components/Card/Card.elements.js
+++ src/components/Card/Card.elements.js
@@ -1,15 +1,40 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group';
+
+export const StyledRadixToggleGroup = styled(ToggleGroupPrimitive.Root)`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    gap: 0 8px;
+    align-items: center;
+` 
+export const StyledRadixItem = styled(ToggleGroupPrimitive.Item)`
+    display: 'flex';
+    border: 0;
+    outline: 0;
+    font-family: inherit;
+    font-size: 1.6em;
+    height: 35px;
+    width: 100%;
+    border-radius: 15px;
+    font-weight: 500;
+    cursor: pointer;
+    color: var(--font-dark);
+    background-color: var(--primary);
+    &:hover { background-color: var(--tertiary);color: var(--font-soft) }
+    &[data-state=on] { background-color: var(--tertiary); color: var(--font-soft)}
+    &:focus { position: 'relative'}
+`
 
 export const CardContainer = styled.div`
     width: 100%;
-    max-width: 500px;
+    max-width: 650px;
     margin: 0 auto;
-    background-color: #fff;
     border-radius: 15px;
     position: relative;
 `
 export const CardInput = styled.input`
-        border-radius: 15px;
+    border-radius: 15px;
     border: 1px solid #66A571;
     color: var(--font-dark);
     width: ${props => props.inputWidth || "100%"};
@@ -25,9 +50,10 @@ export const CardInput = styled.input`
 `
 
 export const CardWrapper = styled.div`
-    padding: 20px 50px;
+    display: block;
+    padding: 26px 50px;
     box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
-    background-color: var(--secundary);
+    background-color: rgb(111 140 67 / 90%);
     border-radius: 0 0 15px 15px;
 `
 
@@ -38,21 +64,38 @@ export const CardItem = styled.div`
     justify-content: ${props => props.justifyContent || "initial"};
 `
 
+export const CardItemContainer = styled.div`
+    opacity: ${props => (props.show ? "1" : "0")};
+    max-height: ${props => (props.show ? "600px" : "0")};
+    overflow: hidden;
+    transition: all 2s ease-in-out;
+`
+
 export const CardTitle = styled.h1`
     font-weight: bold;
     font-size: 2.6em;
     color: var(--font-dark);
     text-align: center;
-    margin: 25px 0 25px 0;
+    padding: 25px 0 25px 0;
+    background-color: rgb(255 255 255 / 90%);
+    border-radius: 15px 15px 0 0;
+`
+export const CardDescription = styled.h2`
+    font-size: 1.8em;
+    font-weight: 400;
+    margin: 20px 0;
+    color: var(--font-soft);
+    min-width: fit-content;
+    margin-right: 15px;
 `
 
-export const CardButton = styled.div`
+export const CardButton = styled.button`
     border-radius: 15px;
     background-color: var(--primary);
     max-width: 155px;
     height: 35px;
     font-size: 1.6em;
-    font-weight: 600;
+    font-weight: 500;
     color: var(--font-dark);
     display: flex;
     align-items: center;
@@ -60,5 +103,13 @@ export const CardButton = styled.div`
     margin: 40px auto auto auto;
     box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
     cursor: pointer;
+    appearance: none;
+    outline: 0;
+    border: 0;
+    padding: 0;
+    width: 100%;
+    font-family: inherit;
 `
+
+
 
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,31 +1,68 @@
-import { CardItem, CardInput, CardButton } from '../../components/Card/Card.elements';
+import React, {useState} from 'react';
+import { CardItem, CardInput, CardButton, CardItemContainer, CardDescription, StyledRadixItem, StyledRadixToggleGroup } from '../../components/Card/Card.elements';
 import { Card } from '../../components/index';
 
+
 export const Register = () => {
+
+    const [visibility, setVisibility] = useState(false);
+    const [userCategory, setUserCategory] = useState(null);
+    //userCategory determina o tipo de cadastro (paciente/nutricionista)
+
+    const displayForm = (userCategory) => {
+        setVisibility(true)
+        setUserCategory(userCategory)
+    }
+    
     return (
         <>
-            <Card cardTitle="Cadastro" cardButton="Cadastrar">
+            <Card cardTitle="Cadastro" >
                 <CardItem>
-                    <CardInput placeholder="Nome" inputWidth="50%"></CardInput>
-                    <CardInput placeholder="Sobrenome" inputWidth="50%"></CardInput>
-                </CardItem>
-                <CardItem>
-                    <CardInput type="mail" placeholder="Email" inputWidth="100%"></CardInput>
-                </CardItem>
-                <CardItem>
-                    <CardInput placeholder="DDD" inputWidth="11%"></CardInput>
-                    <CardInput  placeholder="Telefone" inputWidth="89%"></CardInput>
-                </CardItem>
-                <CardItem>
-                    <CardInput  placeholder="Endereço" inputWidth="100%"></CardInput>
-                </CardItem>
-                <CardItem>
-                    <CardInput type="password" placeholder="Senha" inputWidth="100%"></CardInput>
-                </CardItem>
-                <CardItem>
-                    <CardInput placeholder="Repita sua senha" inputWidth="100%"></CardInput>
-                </CardItem>
-                <CardButton>cadastrar</CardButton>
+                    <CardDescription>Eu sou um:</CardDescription>   
+                    <StyledRadixToggleGroup type="single" aria-label="usuario">
+                        <StyledRadixItem onClick={ () => displayForm(false)} value="paciente" aria-label="Paciente">paciente</StyledRadixItem>
+                        <StyledRadixItem onClick={ () => displayForm(true)} value="nutricionista" aria-label="Nutricionista">nutricionista</StyledRadixItem>
+                    </StyledRadixToggleGroup>
+                </CardItem> 
+                <CardItemContainer show={visibility}>
+                    <CardItem>
+                        <CardDescription>Para concluir o cadastro, preencha o formulário abaixo</CardDescription>   
+                    </CardItem>
+                    <CardItem>
+                        <CardInput placeholder="Nome" inputWidth="50%"></CardInput>
+                        <CardInput placeholder="Sobrenome" inputWidth="50%"></CardInput>
+                    </CardItem>
+                    <CardItem>
+                        <CardInput type="mail" placeholder="Email" inputWidth="100%"></CardInput>
+                    </CardItem>
+                    <CardItem>
+                        <CardInput placeholder="DDD" inputWidth="11%"></CardInput>
+                        <CardInput  placeholder="Telefone" inputWidth="89%"></CardInput>
+                    </CardItem>
+                    <CardItem>
+                        <CardInput placeholder="CPF"></CardInput>
+                    </CardItem>
+                    <CardItem>
+                        <CardInput  placeholder="Endereço" inputWidth="100%"></CardInput>
+                    </CardItem>
+                    {userCategory ? (
+                        <>
+                            <CardItem>
+                                <CardInput  placeholder="CRN" inputWidth="100%"></CardInput>
+                            </CardItem>
+                        </>
+                    ): 
+                    <>
+                    </>
+                    }     
+                    <CardItem>
+                        <CardInput type="password" placeholder="Senha" inputWidth="100%"></CardInput>
+                    </CardItem>
+                    <CardItem>
+                        <CardInput placeholder="Repita sua senha" inputWidth="100%"></CardInput>
+                    </CardItem>
+                    <CardButton>cadastrar</CardButton>
+                </CardItemContainer>  
             </Card>
         </> 
     )
