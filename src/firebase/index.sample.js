54de9ce99beb88a6c265d3c7a0cdd07960641b8f
jose-balbinotti <43188147+jose-balbinotti@users.noreply.github.com>
2022-03-31 22:29:25 -0300

    Ignorando configuração do firebase

---
+++ src/firebase/index.sample.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth";
+
+// Configuração do firebase
+const firebaseConfig = {
+  // config do firebase
+}
+
+// Inicialização do firebase
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth()
+export const db = getFirestore(app)
